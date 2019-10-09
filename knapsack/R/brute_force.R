9d0d1affc0da544025ae74a1fe1b86042e8f49c8
bh1995 <55191733+bh1995@users.noreply.github.com>
2019-10-09 16:11:56 +0200

    "itm"

--- knapsack/R/brute_force.R
+++ knapsack/R/brute_force.R
@@ -1,5 +1,5 @@
 set.seed(42)
-n <- 10
+n <- 20000
 knapsack_objects <-
   data.frame(
     w=sample(1:4000, size = n, replace = TRUE),
@@ -8,31 +8,36 @@ knapsack_objects <-
 
 
 brute_force_knapsack <- function(x, w){
-  if(x = 0 | W = 0){
-    return(0)
-  }
+  stopifnot(x > 0, w > 0)
   
-  i=2 #start row counter so it starts in the first value of x.
-  optimum_value = 0     
-  selected_items = c()
-  weights <- c()
-  values <- c()
-  while(i<=nrow(x)) #keep going through rows until we reach the end.
-  {
-    w <- as.data.frame(combn(x[,1], i)) #makes all combinations of the weights.
-    v <- as.data.frame(combn(x[,2], i)) #makes all combinations of the values.
-    sumw <- colSums(w) #find which is largest.
-    sumv <- colSums(v)
-    weights <- which(sumw<=W) #choose the sum which is largest from all weight combinations.
-    if(length(weights) != 0){ 
-      values <- sumv[weights]
-      optimum_value <- max(values)
-      temp<-which((values) == optimum_value)
-      maxValWghtIdx <- weights[temp]
-      maxValWght <- w[, maxValWghtIdx]
+  #start row counter so it starts in the first value of x, and start empty lists.
+  i=2 
+  most_valuable = 0     
+  choose_item <- c()
+  weight_list <- c()
+  value_list <- c()
+  
+  #keep going through rows until we reach the end.
+  while(i <= nrow(x)){ 
+    
+    w1 <- as.data.frame(combn(x[,1], i)) #make all combinations of the weights.
+    v <- as.data.frame(combn(x[,2], i)) #make all combinations of the values.
+    sum_w <- colSums(w1)
+    sum_v <- colSums(v)
+    weight_list <- which(sum_w <= w) #choose the sum which is largest from all weight combinations.
+    
+    #find item with largest value for every entire weight_list.
+    if(length(weight_list) != 0){ 
+      value_list <- sum_v[weight_list]
+      most_valuable <- max(value_list)
+      t <- which((value_list) == most_valuable)
+      maxValWghtIdx <- weight_list[t]
+      maxValWght <- w1[,maxValWghtIdx]
+      
+      #add number of chosen items with max value to choose_item.
       j <- 1
       while (j<=i){
-        selected_items[j]<-which(X[,1]==maxValWght[j])
+        choose_item[j] <- which(x[,1] == maxValWght[j])
         j=j+1
       }
     }
@@ -40,5 +45,5 @@ brute_force_knapsack <- function(x, w){
     
   }
   
-  return(list(value=round(optimum_value),elements=selected_items))
+  return(list(value = round(most_valuable), elements = choose_item))
 }
