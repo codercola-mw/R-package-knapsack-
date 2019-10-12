cc179e100197e7041dd82aa423170caf96c11cff
mahhaq <mahbuba.haque@ki.se>
2019-10-12 15:31:09 +0200

    "parallel"

--- R/parallel_brute_force_knapsack.R
+++ R/parallel_brute_force_knapsack.R
@@ -1,10 +1,12 @@
+
+
 parallel_brute_force_knapsack <- function(x, W){
-  c <- parralel::detectCores()
+  c <- parallel::detectCores()
   
   n <- length(x$W)
   
-  cluster1 <- makeCluster(c, type = "PSOCK")
-  bmatrix <- parLapply(cluster1, c(1:2^n), function(x){as.integer(intToBits(x)[1:n])})
+  cluster1 <- parallel::makeCluster(c, type = "PSOCK")
+  bmatrix <- parallel::parLapply(cluster1, c(1:2^n), function(x){as.integer(intToBits(x)[1:n])})
   
   
   
