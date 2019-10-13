f8f5e18f4561d87f5d3e7e18d36a42bb440b91e1
bh1995 <55191733+bh1995@users.noreply.github.com>
2019-10-13 19:13:04 +0200

    "parallel"

--- R/parallel_brute_force_knapsack.R
+++ R/parallel_brute_force_knapsack.R
@@ -5,8 +5,25 @@ parallel_brute_force_knapsack <- function(x, W){
   
   n <- length(x$W)
   
-  cluster1 <- parallel::makeCluster(c, type = "PSOCK")
-  bmatrix <- parallel::parLapply(cluster1, c(1:2^n), function(x){as.integer(intToBits(x)[1:n])})
+  cluster1 <- makeCluster(c, type = "PSOCK")
+  bm <- parLapply(cluster1, c(1:2**n), function(x){as.integer(intToBits(x)[1:n])})
+  stopCluster(cluster1)
+  cluster2 <- makeCluster(c, type = "PSOCK")
+  w1 <- simplify2array(parLapply(cluster2, bm, function(y){y%*%x$W}))
+  stop(cluster2)
+  cluster3 <- makeCluster(c, type = "PSOCK")
+  v1 <- simplify2array(parLapply(cluster3, bm, function(y){y%*%x$v}))
+  stopCluster(cluster3)
+  
+  v1[w1 > W]=0
+  maxValIdx <- which.max(v1)
+  
+  elements_Idx <- bm[[maxValIdx]]
+  
+  elements <- c(c(1:n) * elements_Idx)
+  elements <- elements[elements > 0]
+  
+  return(list(value = round(maxValIdx), elements = elements))
   
   
   
